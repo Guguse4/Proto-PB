fc946db94a7b31a7aa5b64cd4f43192094d68ca6
Evan DUBOIS <72149631+Evan-DUBOIS-02@users.noreply.github.com>
2026-02-21 20:59:13 +0100

    Fix player instantiate ?

--- Assets/Scripts/Combat/CombatManager.cs
+++ Assets/Scripts/Combat/CombatManager.cs
@@ -7,10 +7,41 @@ public class CombatManager : NetworkBehaviour
     [SerializeField] private GameObject _playerPrefab;
         
     private GameObject _playerPrefabInstance;
+    private bool _loaded = false;
     
     void Start()
     {
         _playerPrefabInstance = Instantiate(_playerPrefab, transform.position, Quaternion.identity);
         _playerPrefabInstance.GetComponent<NetworkObject>().Spawn();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        NetworkManager.Singleton.SceneManager.OnLoadEventCompleted += (sceneName, loadMode, a, b) =>
+        {
+            if (_loaded)
+                return;
+
+            _loaded = true;
+            if (IsSessionOwner)
+            {
+                SpawnAllPlayerInScene();
+            }
+        };
+    }
+
+    public void SpawnAllPlayerInScene()
+    {
+        foreach(ulong cliendId in NetworkManager.Singleton.ConnectedClientsIds)
+        {
+            SpawnPlayer(cliendId);
+        }
+    }
+
+    public void SpawnPlayer(ulong cliendId)
+    {
+        GameObject player = Instantiate(_playerPrefab, transform.position, Quaternion.identity);
+        player.GetComponent<NetworkObject>().SpawnAsPlayerObject(cliendId);
     }
 }
