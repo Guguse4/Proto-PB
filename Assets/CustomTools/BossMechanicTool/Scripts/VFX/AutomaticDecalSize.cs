ed6e7969db97b106a10f3fdac999e2c0e39ab0bf
Evan DUBOIS <72149631+Evan-DUBOIS-02@users.noreply.github.com>
2026-02-22 09:31:05 +0100

    start decal telegraph

---
+++ Assets/CustomTools/BossMechanicTool/Scripts/VFX/AutomaticDecalSize.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace BossMechanicTool.VFX
+{
+    [RequireComponent(typeof(DecalProjector))]
+    public class AutomaticDecalSize: MonoBehaviour
+    {
+        private void Start()
+        {
+            Vector3 size =  transform.localScale;
+            DecalProjector projector = GetComponent<DecalProjector>();
+            
+            projector.uvScale = new Vector2(projector.uvScale.x *  size.x, projector.uvScale.y *  size.y);
+        }
+    }
+}
