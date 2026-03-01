7db1e631a46e8ece062261461cb8768e92895662
Evan DUBOIS <72149631+Evan-DUBOIS-02@users.noreply.github.com>
2026-03-01 11:33:26 +0100

    Remove player input from timeline manager

--- Assets/CustomTools/BossMechanicTool/Scripts/Timeline/TimelineManager.cs
+++ Assets/CustomTools/BossMechanicTool/Scripts/Timeline/TimelineManager.cs
@@ -1,25 +1,13 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.Playables;
 
 [RequireComponent(typeof(PlayableDirector))]
 public class TimelineManager : MonoBehaviour
 {
-    private PlayerInput playerInput;
-    private InputAction startTimelineAction;
-    
     private PlayableDirector _playableDirector;
     void Start()
     {
         _playableDirector = GetComponent<PlayableDirector>();
-        playerInput = GetComponent<PlayerInput>();
-        startTimelineAction = playerInput.actions.FindAction("StartTimeline");
-    }
-    void Update()
-    {
-        if (startTimelineAction.IsPressed() && _playableDirector.state != PlayState.Playing)
-        {
-            _playableDirector.Play();
-        }
+        _playableDirector.Play();
     }
 }
